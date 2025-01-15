b55d9e44e87fa3f07e5cd7abae2cc6eefb7eec91
chrissda <jhersonbarreto88@hotmail.com>
2025-01-14 21:56:35 -0500

    correcion texto producto

--- src/Views/ProductViewN.jsx
+++ src/Views/ProductViewN.jsx
@@ -26,7 +26,7 @@ const ProductViewN = () => {
       <section className="text-gray-400 bg-indigo-900 body-font overflow-hidden">
         <div className="container px-5 py-24 mx-auto">
           <div className="lg:w-4/5 mx-auto flex flex-wrap">
-            <img alt="product" className="lg:w-1/2 mx-auto w-2/3 lg:h-auto h-auto object-cover object-center rounded-xl" src={foto} />
+            <img alt="Imagen producto" className="lg:w-1/2 mx-auto w-2/3 lg:h-auto h-auto object-cover object-center rounded-xl" src={foto} />
             <div className="lg:w-1/2 w-full lg:pl-10 lg:py-6 mt-6 lg:mt-0">
               <h2 className="text-sm title-font text-gray-500 tracking-widest">CHRISS STORE</h2>
               <h1 className="text-white text-3xl title-font font-medium mb-1">{nombre}</h1>
